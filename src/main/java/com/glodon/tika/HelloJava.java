7b26580fd93df9f8f59f8053f3da04fd2fbfab08
liuzhongkai <sdhzlzhk@126.com>
2016-12-15 00:56:06 +0800

    我在公司提交的代码

--- src/main/java/com/glodon/tika/HelloJava.java
+++ src/main/java/com/glodon/tika/HelloJava.java
@@ -4,7 +4,8 @@ package com.glodon.tika;
  * Created by zhongkai on 2016/12/15.
  */
 public class HelloJava {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("hello git java.");
+        System.out.println("这是我远程修改的");
     }
 }
