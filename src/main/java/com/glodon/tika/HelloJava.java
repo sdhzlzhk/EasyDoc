235c19cef6dc56cc2163acaf37df189c2ec4968b
lzhk <sdhzlzhk@126.com>
2016-12-15 00:44:46 +0800

    push到服务器

---
+++ src/main/java/com/glodon/tika/HelloJava.java
@@ -0,0 +1,10 @@
+package com.glodon.tika;
+
+/**
+ * Created by zhongkai on 2016/12/15.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.print("hello git java.");
+    }
+}
