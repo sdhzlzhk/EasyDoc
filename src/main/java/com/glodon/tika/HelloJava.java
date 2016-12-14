9b4ca08a0e7e4976c113bc8c61431f559e67f8b2
liuzhongkai <sdhzlzhk@126.com>
2016-12-15 01:39:22 +0800

    修改了bug，提交代码

--- src/main/java/com/glodon/tika/HelloJava.java
+++ src/main/java/com/glodon/tika/HelloJava.java
@@ -8,5 +8,6 @@ public class HelloJava {
         System.out.print("hello git java.");
         System.out.println("这是我远程修改的");
         System.out.println("我又修改了代码哦");
+        System.out.println("仲恒修改了代码哦");
     }
 }
