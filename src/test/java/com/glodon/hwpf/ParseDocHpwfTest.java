582dba479a9e20c3171ff28fddd29bb4f4a1f277
lzhk <sdhzlzhk@126.com>
2016-12-20 01:40:38 +0800

    push到服务器

--- src/test/java/com/glodon/hwpf/ParseDocHpwfTest.java
+++ src/test/java/com/glodon/hwpf/ParseDocHpwfTest.java
@@ -1,16 +1,23 @@
 package com.glodon.hwpf;
 
 import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.model.PicturesTable;
 import org.apache.poi.hwpf.usermodel.*;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by zhongkai on 2016/12/17.
  */
 public class ParseDocHpwfTest {
-    private static final String FILE_PATH = "C:\\Users\\liuzk\\Desktop\\sentry调研文档.doc";
+//    private static final String FILE_PATH = "C:\\Users\\liuzk\\Desktop\\sentry调研文档.doc";
+    private static final String FILE_PATH = "C:\\Users\\zhongkai\\Desktop\\sentry调研文档.doc";
+    private static final String IMAGE_DIR = "\\sentry图片";
     public static final String NEXT_PAGE = "\f";
     public static void main(String[] args) throws IOException {
         //TODO first create a HWPFDocument
@@ -30,30 +37,57 @@ public class ParseDocHpwfTest {
         int sections = docRange.numSections();
         int characterRuns = docRange.numCharacterRuns();
         System.out.println("paragraphs = " + paragraphs + "  sections = " + sections +"  characterRuns = " + characterRuns);
+        PicturesTable picturesTable = document.getPicturesTable();
+        List<Picture> pictures = picturesTable.getAllPictures();
         for(int i = 0; i < sections; i++){
             Section curSection = docRange.getSection(i);
             System.out.println("###############Section 第" + (i+1) + " 章 has " + curSection.numSections() + " sub sections.#############");
-            int sectionPara = curSection.numParagraphs();
-            for(int j = 0; j < sectionPara; j++){
+//            picturesTable.extractPicture()
+            for(int j = 0; j < curSection.numParagraphs(); j++){
                 Paragraph paragraph = curSection.getParagraph(j);
                 //TODO: 判断空行
                 if(paragraph.numCharacterRuns() > 1) {
                     String text = paragraph.text();
                     if (text.contains("HYPERLINK") && text.indexOf('\u0013') > -1 && text.indexOf('\u0015') > -1){
                         text = Paragraph.stripFields(text);
-                        System.out.println("styleIndex "+ paragraph.getStyleIndex() + " " + paragraph.numCharacterRuns()  + "  目录：" + Paragraph.stripFields(text));
+                        while (text.indexOf("\u0013") > -1 || text.indexOf("\t") > -1){
+                            int exists13 = text.indexOf("\u0013");
+                            int tab = text.indexOf("\t");
+                            if(exists13 > -1){
+                                text = text.substring(0, exists13);
+                            }
+                            if(tab > -1){
+                                text = text.substring(0, tab);
+                            }
+                        }
+                        System.out.println("styleIndex "+ paragraph.getStyleIndex() + " " + paragraph.numCharacterRuns()  + "  目录：" + text);
                     } else {
-                        System.out.println("styleIndex "+ paragraph.getStyleIndex() + " #" + paragraph.numCharacterRuns() + " StartOffset "+ paragraph.getStartOffset() + "  EndOffset = " + paragraph.getEndOffset() +"   内容：" + Paragraph.stripFields(text));
+                        if(text.indexOf("\u0001") > -1){
+                            String imgPath = null;
+                            for(int k = 0; k < paragraph.numCharacterRuns(); k++){
+                                CharacterRun characterRun = paragraph.getCharacterRun(k);
+                                if(picturesTable.hasPicture(characterRun)){
+                                    Picture picture = picturesTable.extractPicture(characterRun,false);
+                                    imgPath = IMAGE_DIR + UUID.randomUUID().toString() + ".jpg";
+                                    picture.writeImageContent(new FileOutputStream(imgPath));
+                                }
+                            }
+                            System.out.println(paragraph.isWordWrapped() + " 有图片" + imgPath);
+                        } else {
+                            if(paragraph.isInTable()) {
+                                Table table = curSection.getTable(paragraph);
+                                System.out.println("表格:"+table.numRows()+"行");
+                            } else {
+                                System.out.println("styleIndex "+ paragraph.getStyleIndex() + " #" + paragraph.numCharacterRuns() + " StartOffset "+ paragraph.getStartOffset() + "  EndOffset = " + paragraph.getEndOffset() +"   内容：" + Paragraph.stripFields(text));
+                            }
+                        }
                     }
                 }
             }
             System.out.println("##################第 "+(i+1)+" 章结束##################");
-            if(i > 1){
+            /*if(i > 1){
                 break;
-            }
-            String msg1 = "\u0013 TOC \\o \"1-3\" \\h \\z \\u \u0014\u0013 HYPERLINK \\l \"_Toc469917452\" \u0001\u00141 Apache Sentry介绍\t\u0013 PAGEREF _Toc469917452 \\h \u0001\u00141\u0015\u0015\n";
-            String msg2 = "\u0013 HYPERLINK \\l \"_Toc469917453\" \u0001\u00141.1 Sentry介绍\t\u0013 PAGEREF _Toc469917453 \\h \u0001\u00141\u0015\u0015\n";
-
+            }*/
         }
     }
 
